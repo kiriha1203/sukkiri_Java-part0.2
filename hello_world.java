0da8f6932a546cb91152b7f180231eebedda0f6f
kiriha1203 <mizumoto.takefumi@gmail.com>
2020-09-15 20:33:19 +0900

    end

--- hello_world.java
+++ hello_world.java
@@ -1,5 +1,11 @@
-class Sample {
+class Main {
 	public static void main(String[] args) {
 		System.out.println("hello, world!");
+		System.out.println("31 + 31 の計算をします");
+		System.out.println( 31 + 31);
+		System.out.println("答えは" + 62);
+		int x;
+		x = 6;
+		System.out.println(x * x * 3.14);
 	}
 }
